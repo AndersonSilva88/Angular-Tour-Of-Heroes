6a8d94c366429baa49949e5eed0500b2ceff1bff
Anderson Oliveira <anderson.os88@hotmail.com>
2022-06-26 18:20:22 -0300

    observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,27 @@
-import { HEROES } from './../mock-heroes';
+import { HeroService } from './../hero.service';
 import { Hero } from './../hero.model';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent  {
+export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
+  constructor(private HeroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.HeroService.getHeroes().subscribe((heroes) =>
+      (this.heroes = heroes));
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
