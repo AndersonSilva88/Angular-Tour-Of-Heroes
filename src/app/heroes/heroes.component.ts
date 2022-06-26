afb5a0d12731cbc65bc955fecff3968f82f84530
Anderson Oliveira <anderson.os88@hotmail.com>
2022-06-26 15:00:24 -0300

    material

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,20 @@
+import { HEROES } from './../mock-heroes';
 import { Hero } from './../hero.model';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent  {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Wolverine'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolverine' },
+  { id: 2, name: 'Magneto' },
+  { id: 3, name: 'Motoqueiro Fantasma' },
+  { id: 4, name: 'Mulher Maravilha' },
+  { id: 5, name: 'Capitão America' },
+  { id: 6, name: 'Hulk' },
+  { id: 7, name: 'Homen de Ferro' },
+  { id: 8, name: 'Capitão Marvel' },
+  { id: 9, name: 'Dr Estranho' },
+  { id: 10, name: 'Pantera Negra' },
+  { id: 12, name: 'Thor' },
+  { id: 13, name: 'Demolidor' },
+
+];
